e28fe9e70daf74c255340ea069eaa4e7f3a5eeee
subinsong01 <haely97@naver.com>
2024-05-07 17:31:58 +0900

    multi-page

---
+++ 5_02/src/main/java/com/shushub/a5_02/MainActivity.java
@@ -0,0 +1,21 @@
+package com.shushub.a5_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
